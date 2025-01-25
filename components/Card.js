2b152f08134fdd14e17597443f90e1473ab25c85
Leandro Oliveira <leandro.gooli@gmail.com>
2025-01-25 12:11:07 +0000

    Checkpoint antes do styles

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+export default function Card ({name, stock, tags}) {
+    return (
+        <div className=" w-full max-w-xs border-2 rounded-lg shadow-lg p-4 bg-white">
+            <h2 className="text-base font-semibold text-gray-800 md:text-lg">{name}</h2>
+            <p className="text-sm text-gray-600 md:text-base">Stock: <span className="font-medium">{stock} </span></p>
+            <div className="mt-2 flex flex-wrap gap-2">
+                {tags.length > 0 ? (
+                    tags.map((tag,index) => (
+                        <span key={index} className="bg-blue-100 text-blue-800 px-2 py-1 rounded-full text-sm">
+                            {tag} 
+                        </span>
+                    ))
+                ):
+                (
+                    <span className="text-sm text-gray-400 italic ">No tags</span>
+                )}
+            </div>
+
+            <p className="text-sm text-gray-500 mt-2">{tags.length > 0 ? tags.join(", ") : 'No tags"'}</p>
+
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { fetchProducts } from "../lib/api/products";
+import Card from "../components/Card";
 import Link from "next/link";
+import styles from "../styles"
 
 export async function getServerSideProps() {
     try {
@@ -17,22 +19,27 @@ export async function getServerSideProps() {
 
 export default function ProductsPage({ products }) { 
     return (
-        <div>
+        <section className={`${styles.paddings} relative z-10`}>
             <div>
                 <Link href={"/tags"}>All tags</Link>
             </div>
 
-            <h1>Products List Page</h1>
-            <ul>
+            <h1 className="text-2xl font-bold mb-6">Products List Page</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {products.map((product, index) => (
-                    <li key={index}>
-                        <strong>Nome:</strong> {product.name} <br />
-                        <strong>Stock:</strong> {product.stock} <br />
-                        <strong>Tag:</strong> {product.tag.length > 0 ? product.tag.join(', '): 'No tag'} <br />
-                        <br />
-                    </li>
+                    <div key={index} className="flex justify-center">
+                        <Card 
+                            name={product.name}
+                            stock={product.stock}
+                            tags={product.tag} 
+                        />
+                    </div>
+                        // <strong>Nome:</strong> {product.name} <br />
+                        // <strong>Stock:</strong> {product.stock} <br />
+                        // <strong>Tag:</strong> {product.tag.length > 0 ? product.tag.join(', '): 'No tag'} <br />
+                        // <br />
                 ))}
-            </ul>
-        </div>
+            </div>
+        </section>
     );
 }
